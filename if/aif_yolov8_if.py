2d4faef14aeaa530f150fcf83fcddf7254f00fa4
Jason Seawall <jseawall@numurus.com>
2025-02-25 16:17:44 -0800

    3p2p0-RC2

---
+++ if/aif_yolov8_if.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2024 Numurus <https://www.numurus.com>.
+#
+# This file is part of nepi applications (nepi_apps) repo
+# (see https://https://github.com/nepi-engine/nepi_apps)
+#
+# License: nepi applications are licensed under the "Numurus Software License", 
+# which can be found at: <https://numurus.com/wp-content/uploads/Numurus-Software-License-Terms.pdf>
+#
+# Redistributions in source code must retain this top-level comment block.
+# Plagiarizing this software to sidestep the license obligations is illegal.
+#
+# Contact Information:
+# ====================
+# - mailto:nepi@numurus.com
+#
+
+import sys
+import os
+import os.path
+# ROS namespace setup for stand alone testing. Comment out for deployed version
+#NEPI_BASE_NAMESPACE = '/nepi/s2x/'
+#os.environ["ROS_NAMESPACE"] = NEPI_BASE_NAMESPACE[0:-1] # remove to run as automation script
+import rospy
+
+import glob
+import subprocess
+import yaml
+import time
+import numpy as np
+
+
+from nepi_sdk import nepi_ros
+from nepi_sdk import nepi_msg
+
+
+from std_msgs.msg import Empty, Float32, Int32, String, Bool
+
+from nepi_sdk.save_cfg_if import SaveCfgIF
+
+
+TEST_AI_DICT = {
+'description': 'Yolov8 ai framework support', 
+'pkg_name': 'nepi_aif_yolov8', 
+'if_file_name': 'aif_yolov8_if.py', 
+'if_path_name': '/opt/nepi/ros/share/nepi_aifs', 
+'if_module_name': 'aif_yolov8_if', 
+'if_class_name': 'Yolov8AIF', 
+'models_folder_name': 'yolov8', 
+'model_prefix': 'yolov8_', 
+'launch_pkg_name': 'nepi_aif_yolov8',
+'launch_file_name': 'yolov8_ros.launch', 
+'node_file_name': 'nepi_ai_yolov8_detection_node.py',  
+'active': True
+}
+
+TEST_LAUNCH_NAMESPACE = "/nepi/yolov8_test"
+TEST_MGR_NAMESPACE = "/nepi/ai_detector_mgr"
+TEST_MODELS_LIB_PATH = "/mnt/nepi_storage/ai_models/"
+
+
+
+class Yolov8AIF(object):
+    TYPICAL_LOAD_TIME_PER_MB = 3.5
+
+    ai_node_dict = dict()
+    def __init__(self, ai_dict,launch_namespace, mgr_namespace, models_lib_path):
+      if launch_namespace[-1] == "/":
+        launch_namespace = launch_namespace[:-1]
+      self.launch_namespace = launch_namespace  
+      #nepi_msg.printMsgWarn("Launch Namespace: " + self.launch_namespace)
+      if mgr_namespace[-1] == "/":
+        mgr_namespace = mgr_namespace[:-1]
+      self.mgr_namespace = mgr_namespace
+      self.models_lib_path = models_lib_path
+      self.pkg_name = ai_dict['pkg_name']
+      self.node_file_dict = ai_dict['node_file_dict']
+      self.launch_pkg = ai_dict['launch_pkg_name']
+      self.launch_file = ai_dict['launch_file_name']
+      self.model_prefix = ai_dict['model_prefix']
+      self.models_folder = ai_dict['models_folder_name']
+      self.models_folder_path =  os.path.join(self.models_lib_path, self.models_folder)
+      nepi_msg.printMsgInfo("Yolov8 models path: " + self.models_folder_path)
+
+    
+    #################
+    # Yolov8 Model Functions
+
+    def getModelsDict(self):
+        models_dict = dict()
+        # Try to obtain the path to Yolov8 models from the system_mgr
+        nepi_msg.printMsgInfo("ai_yolov8_if: Looking for model files in folder: " + self.models_folder_path)
+        # Grab the list of all existing yolov8 cfg files
+        if os.path.exists(self.models_folder_path) == False:
+            nepi_msg.printMsgInfo("ai_yolov8_if: Failed to find models folder: " + self.models_folder_path)
+            return models_dict
+        else:
+            self.cfg_files = glob.glob(os.path.join(self.models_folder_path,'*.yaml'))
+            nepi_msg.printMsgInfo("ai_yolov8_if: Found network config files: " + str(self.cfg_files))
+            # Remove the ros.yaml file -- that one doesn't represent a selectable trained neural net
+            for f in self.cfg_files:
+                cfg_dict = dict()
+                success = False
+                try:
+                    #nepi_msg.printMsgWarn("ai_yolov8_if: Opening yaml file: " + f) 
+                    yaml_stream = open(f, 'r')
+                    success = True
+                    #nepi_msg.printMsgWarn("ai_yolov8_if: Opened yaml file: " + f) 
+                except Exception as e:
+                    nepi_msg.printMsgWarn("ai_yolov8_if: Failed to open yaml file: " + str(e))
+                if success:
+                    try:
+                        # Validate that it is a proper config file and gather weights file size info for load-time estimates
+                        #nepi_msg.printMsgWarn("ai_yolov8_if: Loading yaml data from file: " + f) 
+                        cfg_dict = yaml.load(yaml_stream)  
+                        model_keys = list(cfg_dict.keys())
+                        model_key = model_keys[0]
+                        #nepi_msg.printMsgWarn("ai_yolov8_if: Loaded yaml data from file: " + f) 
+                    except Exception as e:
+                        nepi_msg.printMsgWarn("ai_yolov8_if: Failed load yaml data: " + str(e)) 
+                        success = False 
+                try: 
+                    #nepi_msg.printMsgWarn("ai_yolov8_if: Closing yaml data stream for file: " + f) 
+                    yaml_stream.close()
+                except Exception as e:
+                    nepi_msg.printMsgWarn("ai_yolov8_if: Failed close yaml file: " + str(e))
+                
+                if success == False:
+                    nepi_msg.printMsgWarn("ai_yolov8_if: File does not appear to be a valid A/I model config file: " + f + "... not adding this model")
+                    continue
+                #nepi_msg.printMsgWarn("ai_yolov8_if: Import success: " + str(success) + " with cfg_dict " + str(cfg_dict))
+                cfg_dict_keys = cfg_dict[model_key].keys()
+                #nepi_msg.printMsgWarn("ai_yolov8_if: Imported model key names: " + str(cfg_dict_keys))
+                #nepi_msg.printMsgWarn("ai_yolov3_if: Imported model key names: " + str(cfg_dict_keys))
+                if ("framework" not in cfg_dict_keys):
+                    nepi_msg.printMsgWarn("ai_yolov8_if: Framework does not specified in model yaml file: " + f + "... not adding this model")
+                    continue
+                if ("weight_file" not in cfg_dict_keys):
+                    nepi_msg.printMsgWarn("ai_yolov8_if: File does not appear to be a valid A/I model config file: " + f + "... not adding this model")
+                    continue
+                #nepi_msg.printMsgWarn("ai_yolov3_if: Imported model key names: " + str(cfg_dict_keys))
+                if ("image_size" not in cfg_dict_keys):
+                    nepi_msg.printMsgWarn("ai_yolov3_if: File does not specify a image size: " + f + "... not adding this model")
+                    continue
+                #nepi_msg.printMsgWarn("ai_yolov3_if: Imported model key names: " + str(cfg_dict_keys))
+                if ("classes" not in cfg_dict_keys):
+                    nepi_msg.printMsgWarn("ai_yolov3_if: File does not specify a classes: " + f + "... not adding this model")
+                    continue
+
+                param_file = os.path.basename(f)
+                framework = cfg_dict[model_key]["framework"]["name"]
+                model_name = self.model_prefix + os.path.splitext(param_file)[0]
+
+                
+                if framework != 'yolov8':
+                    nepi_msg.printMsgWarn("ai_yolov8_if: Model " + model_name + " not a yolov3 model" + framework + "... not adding this model")
+                    continue
+
+
+
+                #nepi_msg.printMsgWarn("ai_yolov8_if: Checking that model weights file exists: " + weight_file_path + " for model name " + model_name)
+                weight_file = cfg_dict[model_key]["weight_file"]["name"]
+                weight_file_path = os.path.join(self.models_folder_path,weight_file)
+                if not os.path.exists(weight_file_path):
+                    nepi_msg.printMsgWarn("ai_yolov8_if: Model " + model_name + " specifies non-existent weights file " + weight_file_path + "... not adding this model")
+                    continue
+                model_type = cfg_dict[model_key]['type']['name']
+                if model_type not in self.node_file_dict.keys():
+                    nepi_msg.printMsgWarn("ai_yolov8_if: Model " + model_name + " specifies non-supported model type " + model_type + "... not adding this model")
+                    continue
+                else:
+                    node_file_name = self.node_file_dict[model_type]
+                model_size = int(os.path.getsize(weight_file_path) / 1000000)
+                model_dict = dict()
+                model_dict['param_file'] = param_file
+                model_dict['framework'] = framework
+                model_dict['model_name'] = model_name
+                model_dict['model_path'] = self.models_folder_path
+                model_dict['type'] = model_type
+                model_dict['description'] = cfg_dict[model_key]['description']['name']
+                model_dict['img_height'] = cfg_dict[model_key]['image_size']['image_height']['value']
+                model_dict['img_width'] = cfg_dict[model_key]['image_size']['image_width']['value']
+                model_dict['classes'] = cfg_dict[model_key]['classes']['names']
+                model_dict['weight_file']= weight_file
+                model_dict['node_file_name'] = node_file_name
+                model_dict['size'] = model_size
+                model_dict['load_time'] = self.TYPICAL_LOAD_TIME_PER_MB * model_size / 1000000
+                nepi_msg.printMsgInfo("ai_yolov8_if: Model dict create for model : " + model_name)
+                models_dict[model_name] = model_dict
+            #nepi_msg.printMsgWarn("Model returning models dict" + str(models_dict))
+        return models_dict
+
+
+    def loadModel(self, model_dict):
+        success = False
+        model_name = model_dict['model_name']
+        node_name = model_name
+        node_namespace = os.path.join(self.launch_namespace, node_name)
+        # Build Darknet new model_name launch command
+        launch_cmd_line = [
+            "roslaunch", self.launch_pkg, self.launch_file,
+            "pkg_name:=" + self.launch_pkg,
+            "node_name:=" + node_name,
+            "node_namespace:=" + self.launch_namespace,
+            "node_file_name:=" + model_dict['node_file_name'],
+            "mgr_namespace:=" + self.mgr_namespace, 
+            "param_file_path:=" + os.path.join(model_dict['model_path'],model_dict['param_file']),
+            "weight_file_path:=" + os.path.join(model_dict['model_path'],model_dict['weight_file'])
+        ]
+        nepi_msg.printMsgInfo("ai_yolov8_if: Launching Yolov8 AI node " + model_name + " with commands: " + str(launch_cmd_line))
+        node_process = subprocess.Popen(launch_cmd_line)
+        self.ai_node_dict[model_name] = {'namesapce':node_namespace, 'process':node_process}
+        success = True
+        return success, node_namespace
+
+
+    def killModel(self,model_name):
+        if model_name in self.ai_node_dict.keys():
+            node_process = self.ai_node_dict[model_name]['process']
+            nepi_msg.printMsgInfo("ai_yolov8_if: Killing Yolov8 AI node: " + model_name)
+            if not (None == node_process):
+                node_process.terminate()
+            del self.ai_node_dict[model_name]
+
+
+ 
+   
+
+if __name__ == '__main__':
+    node_name = "ai_yolov8_test"
+    while nepi_ros.check_for_node(node_name):
+        nepi_msg.printMsgInfo("ai_yolov8_if: Trying to kill running node: " + node_name)
+        nepi_ros.kill_node(node_name)
+        nepi_ros.sleep(2,10)
+    Yolov8AIF(TEST_AI_DICT,TEST_LAUNCH_NAMESPACE,TEST_MGR_NAMESPACE,TEST_MODELS_LIB_PATH)
---
+++ scripts/nepi_ai_yolov8_detection_node.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2024 Numurus <https://www.numurus.com>.
+#
+# This file is part of nepi applications (nepi_apps) repo
+# (see https://https://github.com/nepi-engine/nepi_apps)
+#
+# License: nepi applications are licensed under the "Numurus Software License", 
+# which can be found at: <https://numurus.com/wp-content/uploads/Numurus-Software-License-Terms.pdf>
+#
+# Redistributions in source code must retain this top-level comment block.
+# Plagiarizing this software to sidestep the license obligations is illegal.
+#
+# Contact Information:
+# ====================
+# - mailto:nepi@numurus.com
+#
+
+import os
+import time
+import copy
+import sys
+import rospy
+import torch
+import cv2
+import numpy as np
+
+
+from ultralytics import YOLO
+
+from nepi_sdk import nepi_ros
+from nepi_sdk import nepi_msg
+from nepi_sdk import nepi_ais
+
+from nepi_sdk.ai_detector_if import AiDetectorIF
+
+# Define your PyTorch model and load the weights
+# model = ...
+
+
+TEST_DETECTION_DICT_ENTRY = {
+    'name': 'TEST_DATA', # Class String Name
+    'id': 1, # Class Index from Classes List
+    'uid': '', # Reserved for unique tracking by downstream applications
+    'prob': .3, # Probability of detection
+    'xmin': 10,
+    'ymin': 10,
+    'xmax': 50,
+    'ymax': 50,
+    'width_pixels': 40,
+    'height_pixels': 40,
+    'area_pixels': 16000,
+    'area_ratio': 0.22857
+}
+
+
+
+class Yolov8Detector():
+    defualt_config_dict = {'threshold': 0.3,'max_rate': 5}
+
+    #######################
+    ### Node Initialization
+    DEFAULT_NODE_NAME = "ai_yolov8" # Can be overwitten by luanch command
+    def __init__(self):
+        #### APP NODE INIT SETUP ####
+        nepi_ros.init_node(name= self.DEFAULT_NODE_NAME)
+        self.node_name = nepi_ros.get_node_name()
+        self.base_namespace = nepi_ros.get_base_namespace()
+        self.node_namespace = self.base_namespace + self.node_name
+        nepi_msg.createMsgPublishers(self)
+        nepi_msg.publishMsgInfo(self,"Starting Initialization Processes")
+        ##############################
+        # Initialize parameters and fields.
+        node_params = nepi_ros.get_param(self,"~")
+        nepi_msg.publishMsgInfo(self,"Starting node params: " + str(node_params))
+        self.all_namespace = nepi_ros.get_param(self,"~all_namespace","")
+        if self.all_namespace == "":
+            self.all_namespace = self.node_namespace
+        self.weight_file_path = nepi_ros.get_param(self,"~weight_file_path","")
+        if self.weight_file_path == "" or self.yolov8_path == "":
+            nepi_msg.publishMsgWarn(self,"Failed to get required node info from param server: ")
+            rospy.signal_shutdown("Failed to get valid model info from param")
+        else:
+            # The ai_models param is created by the launch files load network_param_file line
+            model_info = nepi_ros.get_param(self,"~ai_model","")
+            if model_info == "":
+                nepi_msg.publishMsgWarn(self,"Failed to get required model info from params: ")
+                rospy.signal_shutdown("Failed to get valid model file paths")
+            else:
+                try: 
+                    model_framework = model_info['framework']['name']
+                    model_type = model_info['type']['name']
+                    model_description = model_info['description']['name']
+                    self.classes = model_info['classes']['names']
+                    self.img_width = model_info['image_size']['image_width']['value']
+                    self.img_height = model_info['image_size']['image_height']['value']
+                except Exception as e:
+                    nepi_msg.publishMsgWarn(self,"Failed to get required model info from params: " + str(e))
+                    rospy.signal_shutdown("Failed to get valid model file paths")
+
+                if model_framework != 'yolov8':
+                    nepi_msg.publishMsgWarn(self,"Model not a yolov8 model: " + model_framework)
+                    rospy.signal_shutdown("Model not a valid framework")
+
+
+                if model_type != 'detection':
+                    nepi_msg.publishMsgWarn(self,"Model not a valid type: " + model_type)
+                    rospy.signal_shutdown("Model not a valid type")
+
+                nepi_msg.publishMsgInfo(self,"Loading model: " + self.node_name)
+                self.model = YOLO(self.weight_file_path)
+
+                #nepi_msg.publishMsgInfo(self,"Waiting " + str(800) + " seconds for model to load")
+                #nepi_ros.sleep(800)
+
+                nepi_msg.publishMsgInfo(self,"Starting ai_if with defualt_config_dict: " + str(self.defualt_config_dict))
+                self.ai_if = AiDetectorIF(model_name = self.node_name,
+                                    framework = model_framework,
+                                    description = model_description,
+                                    img_height = self.img_height,
+                                    img_width = self.img_width,
+                                    classes_list = self.classes,
+                                    defualt_config_dict = self.defualt_config_dict,
+                                    all_namespace = self.all_namespace,
+                                    processDetectionFunction = self.processDetection)
+
+                #########################################################
+                ## Initiation Complete
+                nepi_msg.publishMsgInfo(self,"Initialization Complete")
+                # Spin forever (until object is detected)
+                nepi_ros.spin()
+                #########################################################        
+              
+
+
+
+    def processDetection(self,cv2_img, threshold):
+        # Example image
+        #img = 'https://ultralytics.com/images/zidane.jpg'
+        # Convert BW image to RGB
+        if cv2_img.shape[2] != 3:
+            cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
+        # Convert BGR image to RGB image
+        cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        cv2_img_shape = cv2_img.shape
+        cv2_img_width = cv2_img_shape[1]
+        cv2_img_height = cv2_img_shape[0]
+        cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
+        #nepi_msg.publishMsgInfo(self,"Original image size: " + str(orig_size))
+
+        # Update model settings
+        self.model.conf = threshold  # Confidence threshold (0-1)
+        #self.model.iou = 0.45  # NMS IoU threshold (0-1)
+        #self.model.max_det = 20  # Maximum number of detections per image
+        #self.model.eval()
+        # Run the detection model on tensor
+
+        try:
+            # Inference
+            results = self.model(cv2_img, conf=threshold)
+            nepi_msg.publishMsgInfo(self,"Got Yolov8 detection result: " + str(results))
+        except Exception as e:
+            nepi_msg.publishMsgInfo(self,"Failed to process img with exception: " + str(e))
+        detect_dict_list = []
+        for i, label in enumurate(results.cls):
+            det_name = label
+            det_id = self.classes.index(det_name)
+            det_prob = results.conf[i]
+            det_box = results.xywhn.[i]
+            
+            detect_dict = {
+                'name': str(label), # Class String Name
+                'id': det_id, # Class Index from Classes List
+                'uid': '', # Reserved for unique tracking by downstream applications
+                'prob': det_prob, # Probability of detection
+                'xmin': det_box[0] - int(det_box[2]/2),
+                'ymin': det_box[1] - int(det_box[3]/2) ,
+                'xmax': det_box[0] + int(det_box[2]/2),
+                'ymax': det_box[1] + int(det_box[3]/2),
+                'width_pixels': img_width,
+                'height_pixels': img_height,
+                'area_pixels': det_box[2] * det_box[3],
+                'area_ratio': (det_box[2] * det_box[3]) / cv2_img_area,
+            }
+            #nepi_msg.publishMsgInfo(self,"Got detect dict entry: " + str(detect_dict))
+
+        return detect_dict_list
+
+
+
+if __name__ == '__main__':
+    Yolov8Detector()
