be43e0c6c71c02b3813497654d6297dbbfa4f569
Jason Seawall <jseawall@numurus.com>
2025-06-08 12:58:05 -0700

    3p2p0-RC6Gf

--- scripts/nepi_ai_yolov8_detection_node.py
+++ scripts/nepi_ai_yolov8_detection_node.py
@@ -104,7 +104,9 @@ class Yolov8Detector():
                 #nepi_sdk.sleep(800)
 
                 self.msg_if.pub_info("Starting ai_if with default_config_dict: " + str(self.default_config_dict))
-                self.ai_if = AiDetectorIF(model_name = self.node_name,
+                self.ai_if = AiDetectorIF(
+                                    namespace = self.node_namespace,
+                                    model_name = self.node_name,
                                     framework = model_framework,
                                     description = model_description,
                                     proc_img_height = self.proc_img_height,
