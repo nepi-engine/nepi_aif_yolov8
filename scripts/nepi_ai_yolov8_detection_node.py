cb097ae3d46e97cd0abe39887dd24dcfa76268e1
Jason Seawall <jseawall@numurus.com>
2025-11-16 11:29:44 -0800

    **

--- scripts/nepi_ai_yolov8_detection_node.py
+++ scripts/nepi_ai_yolov8_detection_node.py
@@ -16,6 +16,9 @@
 # - mailto:nepi@numurus.com
 #
 
+print("-------------------------\nLoading YoloV8 AI DETECTOR Packages\n-------------------------")
+
+
 import os
 import time
 import copy
@@ -106,18 +109,26 @@ class Yolov8Detector():
                     if cuda_count > 0:
                         self.device = 'cuda'
 
-                self.msg_if.pub_info("Loading model: " + self.node_name)
+                self.msg_if.pub_warn("Loading model: " + self.node_name)
                 self.model = YOLO(self.weight_file_path)
 
-                #self.msg_if.pub_info("Waiting " + str(800) + " seconds for model to load")
-                #nepi_sdk.sleep(800)
 
                 # Initialize Detector with Blank Img
-                self.msg_if.pub_info("Initializing detector with blank img")
+                self.msg_if.pub_warn("Initializing detector with blank img")
                 init_cv2_img=nepi_img.create_cv2_blank_img()
-                img_dict=self.preprocessImage(init_cv2_img)
-                det_dict=self.processDetection(img_dict)
+                det_dict=self.processDetection(init_cv2_img)
 
+                # Run Tests
+                NUM_TESTS=10
+                self.msg_if.pub_warn("Running Detection Speed Test on " + str(NUM_TESTS) + " Images")
+                start_time = time.time()
+                for i in range(1, NUM_TESTS):
+                    det_dict=self.processDetection(init_cv2_img)
+                elapsed_time = ( time.time() - start_time )  / 3 # Slower for real images
+                detect_rate = round( float(1.0)/elapsed_time * NUM_TESTS , 2)
+                self.msg_if.pub_warn("Average Detection Rate: " + str(detect_rate) + " hz")
+
+                # Create API IF Class
                 self.msg_if.pub_info("Starting ai_if with default_config_dict: " + str(self.default_config_dict))
                 self.ai_if = AiDetectorIF(
                                     namespace = self.node_namespace,
@@ -129,7 +140,6 @@ class Yolov8Detector():
                                     classes_list = self.classes,
                                     default_config_dict = self.default_config_dict,
                                     all_namespace = self.all_namespace,
-                                    preprocessImageFunction = self.preprocessImage,
                                     processDetectionFunction = self.processDetection,
                                     has_img_tiling = False)
 
@@ -143,110 +153,103 @@ class Yolov8Detector():
 
 
 
-    def preprocessImage(self,cv2_img,options_dict=dict()):
-        height, width = cv2_img.shape[:2]
-        
-        # For Future
-        '''
-        tile = False
-        if 'tile'  in options_dict.keys():
-            tile = options_dict['tile']
-        '''
-        [cv2_img,ratio,new_width,new_height] = nepi_img.resize_proportionally(cv2_img, self.proc_img_width,self.proc_img_height,interp = cv2.INTER_NEAREST)
-        
-        # Convert BW image to RGB
-        if nepi_img.is_gray(cv2_img):
-            cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
 
-        #self.msg_if.pub_info(":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
-        # Create image dict with new image
-        img_dict = dict()
-        img_dict['cv2_img'] = cv2_img
-        img_shape = cv2_img.shape
-        img_dict['image_width'] = width 
-        img_dict['image_height'] = height 
-        img_dict['prc_width'] = new_width 
-        img_dict['prc_height'] = new_height 
-        img_dict['ratio'] = ratio 
+    def processDetection(self, cv2_img, img_dict=dict(), threshold = 0.3, resize = False, verbose = False):
+
+        img_dict['image_width'] = 1
+        img_dict['image_height'] = 1 
+        img_dict['prc_width'] = 1
+        img_dict['prc_height'] = 1 
+        img_dict['ratio'] = 1
         img_dict['tiling'] = False
 
-        return img_dict
-
-
-
-    def processDetection(self,img_dict, threshold = 0.3):
         detect_dict_list = []
-        tile = False
-        # For Future
-        #if 'tile'  in options_dict.keys():
-        #    tile = options_dict['tile']
-        if img_dict is not None:
-            if 'cv2_img' in img_dict.keys():
-                cv2_img = img_dict['cv2_img']
-                if cv2_img is not None:
+        if cv2_img is not None:
 
-                    # Convert BGR image RGB
+                cv2_img_shape = cv2_img.shape
+                cv2_img_width = cv2_img_shape[1]
+                cv2_img_height = cv2_img_shape[0]
+                cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
+
+                if resize == True:
+                    [cv2_img,rescale_ratio,prc_width,prc_height] = nepi_img.resize_proportionally(cv2_img, self.proc_img_width,self.proc_img_height,interp = cv2.INTER_NEAREST)
+                else:
+                    rescale_ratio = 1
+                    prc_width = cv2_img_width
+                    prc_height = cv2_img_height
+
+                # Convert to RGB
+                if nepi_img.is_gray(cv2_img):
+                    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
+                else:
                     cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-                    cv2_img_shape = cv2_img.shape
-                    cv2_img_width = cv2_img_shape[1]
-                    cv2_img_height = cv2_img_shape[0]
-                    cv2_img_area = cv2_img_shape[0] * cv2_img_shape[1]
+
+                #self.msg_if.pub_info(":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
+                # Create image dict with new image
+                img_dict['image_width'] = cv2_img_width 
+                img_dict['image_height'] = cv2_img_height 
+                img_dict['prc_width'] = prc_width 
+                img_dict['prc_height'] = prc_height 
+                img_dict['ratio'] = rescale_ratio 
+                img_dict['tiling'] = False
 
 
-                    # Update model settings
-                    self.model.conf = threshold  # Confidence threshold (0-1)
+                # Update model settings
+                self.model.conf = threshold  # Confidence threshold (0-1)
 
-                    try:
-                        # Inference
-                        start_time = nepi_sdk.get_time()
+                try:
+                    # Inference
+                    start_time = nepi_sdk.get_time()
 
-                        results = self.model(cv2_img, conf=threshold, verbose=False) #, device=self.device)
-                        
-                        detect_time = round( (nepi_sdk.get_time() - start_time) , 3)
-                        #self.msg_if.pub_info("Detector Detect Time: {:.2f}".format(detect_time))
+                    results = self.model(cv2_img, conf=threshold, verbose=False) #, device=self.device)
 
-                        #self.msg_if.pub_warn("Got Yolov8 detection results: " + str(results[0].boxes))
+                    detect_time = round( (nepi_sdk.get_time() - start_time) , 3)
+
+
+                    ids = results[0].boxes.cls.to('cpu').tolist()
+                    boxes = results[0].boxes.xyxy.to('cpu').tolist()
+                    confs = results[0].boxes.conf.to('cpu').tolist()
+
+                    # self.msg_if.pub_warn("Got detection ids: " + str(ids))
+                    # self.msg_if.pub_warn("Got detection boxes: " + str(boxes))
+                    # self.msg_if.pub_warn("Got detection confs: " + str(confs))
                 
-                        #self.msg_if.pub_warn("Got Yolov8 detection results: " + str(results[0].boxes))
-                        ids = results[0].boxes.cls.to('cpu').tolist()
-                        #self.msg_if.pub_warn("Got Yolov8 detection ids: " + str(ids))
-                        boxes = results[0].boxes.xyxy.to('cpu').tolist()
-                        #self.msg_if.pub_warn("Got Yolov8 detection boxes: " + str(boxes))
-                        confs = results[0].boxes.conf.to('cpu').tolist()
-                        #self.msg_if.pub_warn("Got Yolov8 detection confs: " + str(confs))
-                    
-                    except Exception as e:
-                        self.msg_if.pub_info("Failed to process detection with exception: " + str(e))
-                
-                    rescale_ratio = float(1) / img_dict['ratio']
-                    for i, idf in enumerate(ids):
-                        id = int(idf)
-                        det_name = self.classes[id]
-                        det_id = id
-                        det_prob = confs[i]
-                        det_box = boxes[i]
-                        det_area = (det_box[2] - det_box[0]) * (det_box[3] - det_box[1])
-                        detect_dict = {
-                            'name': det_name, # Class String Name
-                            'id': det_id, # Class Index from Classes List
-                            'uid': '', # Reserved for unique tracking by downstream applications
-                            'prob': det_prob, # Probability of detection
-                            'xmin': int(det_box[0] ),
-                            'ymin': int(det_box[1] ) ,
-                            'xmax': int(det_box[2] ),
-                            'ymax': int(det_box[3]),
-                            'area_pixels': int(det_area),
-                            'area_ratio': det_area / cv2_img_area
-                        }
-                        # Rescale to orig image size
-                        detect_dict['xmin'] = int(detect_dict['xmin'] * rescale_ratio)
-                        detect_dict['ymin'] = int(detect_dict['ymin'] * rescale_ratio)
-                        detect_dict['xmax'] = int(detect_dict['xmax'] * rescale_ratio)
-                        detect_dict['ymax'] = int(detect_dict['ymax'] * rescale_ratio)
-                        detect_dict_list.append(detect_dict)
-                        #self.msg_if.pub_info("Got detect dict entry: " + str(detect_dict))
+                except Exception as e:
+                    self.msg_if.pub_info("Failed to process detection with exception: " + str(e))
             
-        return detect_dict_list
+                
+                for i, idf in enumerate(ids):
+                    id = int(idf)
+                    det_name = self.classes[id]
+                    det_id = id
+                    det_prob = confs[i]
+                    det_box = boxes[i]
+                    det_area = (det_box[2] - det_box[0]) * (det_box[3] - det_box[1])
+                    detect_dict = {
+                        'name': det_name, # Class String Name
+                        'id': det_id, # Class Index from Classes List
+                        'uid': '', # Reserved for unique tracking by downstream applications
+                        'prob': det_prob, # Probability of detection
+                        'xmin': int(det_box[0] ),
+                        'ymin': int(det_box[1] ) ,
+                        'xmax': int(det_box[2] ),
+                        'ymax': int(det_box[3]),
+                        'area_pixels': int(det_area),
+                        'area_ratio': det_area / cv2_img_area
+                    }
+                    # Rescale to orig image size
+                    detect_dict['xmin'] = int(detect_dict['xmin'] * rescale_ratio)
+                    detect_dict['ymin'] = int(detect_dict['ymin'] * rescale_ratio)
+                    detect_dict['xmax'] = int(detect_dict['xmax'] * rescale_ratio)
+                    detect_dict['ymax'] = int(detect_dict['ymax'] * rescale_ratio)
+                    detect_dict_list.append(detect_dict)
+
+
+                    if verbose == True:
+                        self.msg_if.pub_info("Detector Detect Time: " + str(detect_time))
+                        self.msg_if.pub_info("Got detect dict entry: " + str(detect_dict))
+            
+        return [detect_dict_list, img_dict]
 
 
 
